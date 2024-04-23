5bd0829eb8b9a2bb7309f30ce1774a815c4b3c76
Al1nuX <cryptlabs@gmail.com>
2024-04-23 22:19:02 +0300

    Cleaned the main.py file.

--- main.py
+++ main.py
@@ -1,45 +1,39 @@
-from hashlib import sha256
 import json
 import time
-
-data = b"Hello, this is a test string"
-
-print(sha256(data).hexdigest())
-
+from hashlib import sha256
 
 
 class PyBlock:
     def __init__(self, index, transactions, timestamp, previous_hash):
-            self.index = index 
-            self.transactions = transactions 
-            self.timestamp = timestamp
-            self.previous_hash = previous_hash
-    
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
 
     def compute_hash(self):
-     
+
         block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
 
+
 class PyBlockchain:
- 
+
     def __init__(self):
         self.chain = []
         self.create_start_block()
- 
+
     def create_start_block(self):
 
         start_block = PyBlock(0, [], time.time(), "0")
         start_block.hash = start_block.compute_hash()
         self.chain.append(start_block)
- 
+
     @property
     def last_block(self):
-      
+
         return self.chain[-1]
-    
-    
+
 
 myblockchain = PyBlockchain()
-    
+
 print(PyBlockchain.last_block)
