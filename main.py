008354f566ef8b2d940d4142316b96857e6fce3d
Al1nuX <cryptlabs@gmail.com>
2024-04-23 22:13:38 +0300

    Created the blockhain classes.

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-class Block:
-    def __init__(self, index, transactions, timestamp):
-            """
-            Constructor for the `Block` class.
-            :param index: Unique ID of the block.
-            :param transactions: List of transactions.
-            :param timestamp: Time of generation of the block.
-            """
+from hashlib import sha256
+import json
+import time
+
+data = b"Hello, this is a test string"
+
+print(sha256(data).hexdigest())
+
+
+
+class PyBlock:
+    def __init__(self, index, transactions, timestamp, previous_hash):
             self.index = index 
             self.transactions = transactions 
             self.timestamp = timestamp
-        
+            self.previous_hash = previous_hash
+    
+
+    def compute_hash(self):
+     
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
+
+class PyBlockchain:
+ 
+    def __init__(self):
+        self.chain = []
+        self.create_start_block()
+ 
+    def create_start_block(self):
+
+        start_block = PyBlock(0, [], time.time(), "0")
+        start_block.hash = start_block.compute_hash()
+        self.chain.append(start_block)
+ 
+    @property
+    def last_block(self):
+      
+        return self.chain[-1]
+    
+    
+
+myblockchain = PyBlockchain()
+    
+print(PyBlockchain.last_block)
